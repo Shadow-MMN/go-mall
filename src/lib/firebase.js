95aa93f2ee0f88cd78c92502874c38407acd6d78
Shadow-MMN <nnajimakuochukwu4@example.com>
2025-05-05 18:44:05 +0100

    used .env.local file to protect my api key

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3Gfdilz7cVoyOaStBPA1qfo-9D_nJhoU",
-  authDomain: "go-mall-cf10d.firebaseapp.com",
-  projectId: "go-mall-cf10d",
-  storageBucket: "go-mall-cf10d.appspot.com",
-  messagingSenderId: "1095172279152",
-  appId: "1:1095172279152:web:32114126bb8601af2cddb5",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 
