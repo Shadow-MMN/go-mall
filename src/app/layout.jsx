e209e91db29a41570116c4654ace992ccd464d6b
Shadow-MMN <nnajimakuochukwu4@example.com>
2025-05-06 20:15:31 +0100

    Gave the body a max weight propery and centered it horizontally

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -32,7 +32,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full">
-      <body className="min-h-screen flex flex-col">
+      <body className="min-h-screen flex flex-col max-w-[2000px] mx-auto">
         
         {/* Header at the top */}
         <header>
